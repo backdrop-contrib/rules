b1e08ba53a872f339c71ccf123ac2f72952b38ab
laryn <laryn@cedc.org>
2021-11-01 11:49:04 -0500

    Issue #88: Merge changes from Rules in D7 Fixes #88

--- rules.api.php
+++ rules.api.php
@@ -6,7 +6,7 @@
  *
  * This file contains no working PHP code; it exists to provide additional
  * documentation for doxygen as well as to document hooks in the standard
- * Drupal manner.
+ * Backdrop manner.
  */
 
 /**
